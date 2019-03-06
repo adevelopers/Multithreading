f14b624dedb4b2a2e2706c68af0f7e05a1604a38
Kirill Khudyakov <hudiakov@gmail.com>
2019-03-07 00:28:35 +0300

    Presented MainViewController from AppDelegate

--- Multithreading/Multithreading/AppDelegate.swift
+++ Multithreading/Multithreading/AppDelegate.swift
@@ -15,8 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
-        
-        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        guard let window = window else { return false }
+        let mainViewController = MainViewController()
+        window.rootViewController = mainViewController
+        window.makeKeyAndVisible()
         return true
     }
 
