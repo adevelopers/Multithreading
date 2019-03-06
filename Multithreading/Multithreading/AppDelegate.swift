0eeec65812b3cc470654836070ae5932663b6e0b
Kirill Khudyakov <hudiakov@gmail.com>
2019-03-06 22:14:43 +0300

    Init Xcode project

---
+++ Multithreading/Multithreading/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Multithreading
+//
+//  Created by Kirill Khudiakov on 06/03/2019.
+//  Copyright © 2019 Kirill Khudiakov. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        
+        
+        return true
+    }
+
+}
+
---
+++ Multithreading/Multithreading/Screens/Main/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Multithreading
+//
+//  Created by Kirill Khudiakov on 06/03/2019.
+//  Copyright © 2019 Kirill Khudiakov. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
